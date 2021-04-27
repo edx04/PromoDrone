24d2d20a49df9e0d2927e4eff8bc07da9f311437
Edgar Arellano <A01631335@itesm.mx>
2021-04-27 18:35:03 -0500

    script modelo version 2

---
+++ modelo.py
@@ -0,0 +1,73 @@
+import boto3
+import io
+from PIL import Image, ImageDraw, ExifTags, ImageColor,ImageFont
+
+def display_image(bucket,photo,response):
+    # Load image from S3 bucket
+    s3_connection = boto3.resource('s3')
+
+    s3_object = s3_connection.Object(bucket,photo)
+    s3_response = s3_object.get()
+
+    stream = io.BytesIO(s3_response['Body'].read())
+    image=Image.open(stream)
+
+    # Ready image to draw bounding boxes on it.
+    imgWidth, imgHeight = image.size
+    draw = ImageDraw.Draw(image)
+
+    # calculate and display bounding boxes for each detected custom label
+    print('Detected custom labels for ' + photo)
+    for customLabel in response['CustomLabels']:
+        print('Label ' + str(customLabel['Name']))
+        print('Confidence ' + str(customLabel['Confidence']))
+        if 'Geometry' in customLabel:
+            box = customLabel['Geometry']['BoundingBox']
+            left = imgWidth * box['Left']
+            top = imgHeight * box['Top']
+            width = imgWidth * box['Width']
+            height = imgHeight * box['Height']
+
+            fnt = ImageFont.truetype('/Library/Fonts/arial.ttf', 15)
+            draw.text((left,top), customLabel['Name'], fill='#00d400', font=fnt)
+
+            print('Left: ' + '{0:.0f}'.format(left))
+            print('Top: ' + '{0:.0f}'.format(top))
+            print('Label Width: ' + "{0:.0f}".format(width))
+            print('Label Height: ' + "{0:.0f}".format(height))
+
+            points = (
+                (left,top),
+                (left + width, top),
+                (left + width, top + height),
+                (left , top + height),
+                (left, top))
+            draw.line(points, fill='#00d400', width=5)
+
+    image.show()
+    image.save("aa.jpg")
+
+def show_custom_labels(model,bucket,photo, min_confidence):
+    client=boto3.client('rekognition')
+
+    #Call DetectCustomLabels
+    response = client.detect_custom_labels(Image={'S3Object': {'Bucket': bucket, 'Name': photo}},
+        MinConfidence=min_confidence,
+        ProjectVersionArn=model)
+
+    # For object detection use case, uncomment below code to display image.
+    display_image(bucket,photo,response)
+
+    return len(response['CustomLabels'])
+
+def main():
+    bucket="promodronefaces"
+    photo="test/c13.jpg"
+    model='arn:aws:rekognition:us-east-2:864156931594:project/FacesPromoDrone/version/FacesPromoDrone.2021-04-26T21.12.35/1619489555582'
+    min_confidence = 50
+    label_count=show_custom_labels(model,bucket,photo, min_confidence)  
+    print("Custom labels detected: " + str(label_count))
+
+
+if __name__ == "__main__":
+    main()  
